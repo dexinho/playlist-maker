6f6067b84a03c6c3692fb036b6b47821afdedbb9
Jasmin Hidanovic <dexinho008@gmail.com>
2024-03-11 14:24:53 +0100

    updated styles

--- src/Components/Track/Track.jsx
+++ src/Components/Track/Track.jsx
@@ -1,45 +1,47 @@
 import { Button, Box } from "@mui/material";
 import React from "react";
 
-const Track = ({ tracks, onTrackAdd }) => {
-  if (tracks.length < 1) return <div>No tracks found</div>;
+const Track = ({ tracks, selectedTrackIds, onTrackAdd }) => {
+  if (tracks.length < 1) return <Box>No tracks found.</Box>;
 
   const handleTrackClick = (track) => {
-    onTrackAdd({
-      id: Date.now(),
-      artist: track.artists[0].name,
-      songName: track.name,
-    });
+    onTrackAdd(track);
   };
 
   return (
-    <div className="grid grid-cols-2 gap-2 flex-col p-4 border">
+    <Box className="grid grid-cols-2 gap-2 flex-col p-4">
       {tracks.map((track, index) => (
         <Button
           variant="contained"
           key={index}
           className="flex flex-col gap-4"
+          color={selectedTrackIds[track.id] ? `error` : `primary`}
           onClick={() => handleTrackClick(track)}
         >
-          <Box className="w-full h-20 grid grid-cols-2 gap-4">
-            <img
-              src={track.album.images[0].url}
-            ></img>
-            <div className="text-pretty">
-              <div>Artist: {track.artists[0].name}</div>
-              <div>Song Name: {track.name}</div>
-              <div>Platform: {track.uri.split(":")[0]}</div>
-            </div>
+          <Box className="w-full flex flex-col gap-4">
+            <img src={track.image_url}></img>
+            <Box className="text-pretty">
+              <Box>
+                Artist: <span className="text-black">{track.artist_name}</span>
+              </Box>
+              <Box>
+                Song Name: <span className="text-black">{track.song_name}</span>
+              </Box>
+            </Box>
           </Box>
 
-          <div className="w-full">
+          <Box className="w-full">
             {track.preview_url && (
-              <audio controls src={track.preview_url} className="w-full h-4"></audio>
+              <audio
+                controls
+                src={track.preview_url}
+                className="w-full h-4"
+              ></audio>
             )}
-          </div>
+          </Box>
         </Button>
       ))}
-    </div>
+    </Box>
   );
 };
 
