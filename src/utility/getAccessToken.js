7630938b20e7cec46bda990b03b6aca5cb4fd3ec
Jasmin Hidanovic <dexinho008@gmail.com>
2024-03-14 03:56:00 +0100

    added both search and access

--- src/utility/getAccessToken.js
+++ src/utility/getAccessToken.js
@@ -1,6 +1,13 @@
 import fecthData from "./fetchData";
 
-const getAccessToken = async ({ url, options, isFetching, setIsFetching }) => {
+const getAccessToken = {};
+
+const getAccessTokenForSearch = async ({
+  url,
+  options,
+  isFetching,
+  setIsFetching,
+}) => {
   try {
     const res = await fecthData({
       url,
@@ -12,7 +19,7 @@ const getAccessToken = async ({ url, options, isFetching, setIsFetching }) => {
     if (res.ok) {
       const data = await res.json();
 
-      console.log(data)
+      console.log(data);
 
       return data.access_token;
     }
@@ -21,6 +28,39 @@ const getAccessToken = async ({ url, options, isFetching, setIsFetching }) => {
   }
 };
 
+async function getAccessTokenForPlaylist({
+  code,
+  client_id,
+  client_secret,
+  redirect_uri,
+}) {
+  const auth = base64Encode(`${client_id}:${client_secret}`);
+
+  const params = new URLSearchParams();
+  params.append("grant_type", "authorization_code");
+  params.append("code", code);
+  params.append("redirect_uri", redirect_uri);
+  const body = params.toString();
+
+  const response = await fetch("https://accounts.spotify.com/api/token", {
+    method: "POST",
+    headers: {
+      Authorization: 1,
+      "Content-Type": "application/x-www-form-urlencoded",
+    },
+    body: 1,
+  });
+
+  const data = await response.json();
+  return data.access_token;
+}
+
+function base64Encode(str) {
+  const encodedString = btoa(str);
+  return encodedString;
+}
+
+getAccessToken.search = getAccessTokenForSearch;
+getAccessToken.access = getAccessTokenForPlaylist;
+
 export default getAccessToken;
-
-
