eee0e803a2cc0cf134f57e3a9b9f5d4b52c65c82
Jasmin Hidanovic <dexinho008@gmail.com>
2024-03-14 03:55:19 +0100

    creates playlist

--- src/utility/createPlaylist.js
+++ src/utility/createPlaylist.js
@@ -1,21 +1,35 @@
-async function createPlaylist({ access_token, user_id, playlist_name }) {
+import fecthData from "./fetchData";
+
+async function createPlaylist({
+  access_token,
+  user_id,
+  playlist_name,
+  isFetching,
+  setIsFetching,
+}) {
   try {
-    const response = await fetch(
-      `https://api.spotify.com/v1/users/${user_id}/playlists`,
-      {
-        method: "POST",
-        headers: {
-          Authorization: `Bearer ${access_token}`,
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          name: playlist_name,
-          public: true,
-        }),
-      }
-    );
+    const url = `https://api.spotify.com/v1/users/${user_id}/playlists`;
+    const options = {
+      method: "POST",
+      headers: {
+        Authorization: `Bearer ${access_token}`,
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        name: playlist_name,
+        public: true,
+      }),
+    };
+
+    const response = await fecthData({
+      url,
+      options,
+      isFetching,
+      setIsFetching,
+    });
 
     const data = await response.json();
+
     return data.id;
   } catch (err) {
     console.log(err);
