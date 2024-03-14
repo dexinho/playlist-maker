617133025ae7ea8ad48dfc16adfe05ac1c5ce6e6
Jasmin Hidanovic <dexinho008@gmail.com>
2024-03-14 03:54:09 +0100

    aded playlist refresh

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,11 @@
 import React, { useEffect, useState, useRef } from "react";
-import { Box, CircularProgress, TextField, Typography } from "@mui/material";
+import {
+  Box,
+  Button,
+  CircularProgress,
+  TextField,
+  Typography,
+} from "@mui/material";
 import Track from "./Components/Track/Track";
 import Playlist from "./Components/Playlist/Playlist";
 import getApiKeys from "./utility/getApiKeys";
@@ -8,58 +14,34 @@ import searchTracks from "./utility/searchTracks";
 import createPlaylist from "./utility/createPlaylist";
 import getAuthorizationUrl from "./utility/getAuthorizationUrl";
 import getUserProfile from "./utility/getUserProfile";
-import getAccessTokenForPlaylist from "./utility/getAccessTokenForPlaylist";
-
-const user_id = "31fnwumqzhtohbvkxcjglctzgzpq";
+import addTracksToPlaylist from "./utility/addTracksToPlaylist";
+import delayMS from "./utility/delayMS";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSyncAlt } from "@fortawesome/free-solid-svg-icons";
 
 const App = () => {
-  const [isFetching, setIsFetching] = useState(false);
+  const [isFetching, setIsFetching] = useState({
+    trackSearch: false,
+    trackUpload: false,
+    playlistUpload: false,
+    apiKeys: false,
+    userProfile: false,
+  });
   const [selectedTrackIds, setSelectedTrackIds] = useState({});
   const [apiKeys, setApiKeys] = useState();
   const [accessTokens, setAccessTokens] = useState({});
   const [searchInput, setSearchInput] = useState("");
   const [fetchedTracks, setFetchedTracks] = useState([]);
   const [playlist, setPlaylist] = useState([]);
+  const [userIds, setUserIds] = useState("");
+  const [isAccessGranted, setIsAccessGranted] = useState(false);
+
   const clientSecretInputRef = useRef(null);
   const clientIdInputRef = useRef(null);
 
   useEffect(() => {
-    const apiSetup = async () => {
-      const apiKeys = await getApiKeys({
-        url: "http://localhost:3000/api/spotify",
-        setApiKeys,
-        isFetching,
-        setIsFetching,
-      });
-
-      const spotifyAccessTokenSettings = {
-        url: `https://accounts.spotify.com/api/token`,
-        options: {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/x-www-form-urlencoded",
-          },
-          body: `grant_type=client_credentials&client_id=${apiKeys.spotify.client_id}&client_secret=${apiKeys.spotify.client_secret}`,
-        },
-      };
-
-      const spotifyAccessToken = await getAccessToken({
-        url: spotifyAccessTokenSettings.url,
-        options: spotifyAccessTokenSettings.options,
-        isFetching,
-        setIsFetching,
-      });
-
-      setAccessTokens((prevT) => ({
-        ...prevT,
-        spotify: spotifyAccessToken,
-      }));
-    };
-
-    apiSetup();
     // const client_id = "404cae06b37c4e3897ab0dbfec5bd047";
     // const client_secret = "d3b356e7507a4cc5a44fa8fe9942f64c"
-
     // const tidalAccessTokenSettings = {
     //   url: `https://auth.tidal.com/v1/oauth2/token`,
     //   options: {
@@ -74,7 +56,6 @@ const App = () => {
     //     }),
     //   },
     // };
-
     // getAccessToken({
     //   tidalAccessTokenSettings: { url },
     //   tidalAccessTokenSettings: { options },
@@ -92,8 +73,11 @@ const App = () => {
         url: `https://api.spotify.com/v1/search?q=${encodeURIComponent(
           searchInput
         )}&type=track`,
-        accessToken: accessTokens.spotify,
-        isFetching,
+        accessToken: accessTokens.spotify_search,
+        isFetching: {
+          name: "trackSearch",
+          state: isFetching.trackSearch,
+        },
         setIsFetching,
       });
 
@@ -115,69 +99,6 @@ const App = () => {
       );
 
       setSearchInput("");
-
-      const authHref = getAuthorizationUrl({
-        scopes: "playlist-modify-public",
-        client_id: apiKeys.spotify.client_id,
-        redirect_uri: "http://localhost:3000/callback",
-        response_type: "code",
-      });
-
-      const a = document.createElement("a");
-      a.href = authHref;
-      a.target = "_blank";
-      a.click();
-
-      let isPreparingFile = true;
-      let attempts = 0;
-
-      while (isPreparingFile && attempts <= 100) {
-        const res = await fetch("http://localhost:3000/isFileReady");
-        const status = res.status;
-        console.log(res.status);
-        attempts++;
-
-        if (res.status === 200) break;
-      }
-
-      const codeRes = await fetch("http://localhost:3000/code");
-      const data = await codeRes.json();
-
-      const tokenRes = await fetch("http://localhost:3000/token", {
-        method: "POST",
-        body: JSON.stringify({
-          client_id: apiKeys.spotify.client_id,
-          client_secret: apiKeys.spotify.client_secret,
-          redirect_uri: "http://localhost:3000/callback",
-          code: data.code,
-        }),
-        headers: {
-          "Content-Type": "application/json",
-        },
-      });
-
-      const tokenREADY = await tokenRes.json();
-
-      console.log(tokenREADY);
-
-      // console.log("code", data);
-
-      // const token = await getAccessTokenForPlaylist({
-      //   code: data.code,
-      //   client_id: apiKeys.spotify.client_id,
-      //   client_secret: apiKeys.spotify.client_secret,
-      //   redirect_uri: "http://localhost:3000/callback",
-      // });
-
-      // console.log("token", token);
-
-      // const profile = await getUserProfile({
-      //   access_token: token,
-      //   isFetching,
-      //   setIsFetching,
-      // });
-
-      // console.log("token", profile);
     }
   };
 
@@ -193,20 +114,154 @@ const App = () => {
     setSelectedTrackIds((prevS) => ({ ...prevS, [trackId]: false }));
   };
 
-  const onPlaylistUpload = async (playlist_name) => {
-    createPlaylist({
-      access_token: accessTokens.spotify,
-      user_id,
-      playlist_name,
-    });
+  const onPlaylistUpload = async ({ playlist_name }) => {
+    const trackURIs = playlist.reduce(
+      (acc, track) => [...acc, track.track_uri],
+      []
+    );
+
+    try {
+      const playlistId = await createPlaylist({
+        access_token: accessTokens.spotify_access,
+        user_id: userIds.spotify,
+        playlist_name,
+        isFetching: {
+          name: "createPlaylist",
+          state: isFetching.createPlaylist,
+        },
+        setIsFetching,
+      });
+
+      await addTracksToPlaylist({
+        access_token: accessTokens.spotify_access,
+        playlist_id: playlistId,
+        track_URIs: trackURIs,
+        isFetching: {
+          name: "trackUpload",
+          state: isFetching.trackUpload,
+        },
+        setIsFetching,
+      });
+    } catch (err) {
+      console.log(err);
+    }
   };
 
-  const delayMS = (ms) => {
-    return new Promise((res) => {
-      setTimeout(() => {
-        res();
-      }, ms);
+  const handleGrantAccessClick = async () => {
+    let clientId = clientIdInputRef.current.value;
+    let clientSecret = clientSecretInputRef.current.value;
+
+    if (!clientId || !clientSecret) {
+      const fetchedApiKeys = await getApiKeys({
+        url: "http://localhost:3000/api/spotify",
+        isFetching: {
+          name: "apiKeys",
+          state: isFetching.apiKeys,
+        },
+        setIsFetching,
+      });
+
+      setApiKeys(fetchedApiKeys);
+
+      clientId = fetchedApiKeys.spotify.client_id;
+      clientSecret = fetchedApiKeys.spotify.client_secret;
+    }
+
+    const spotifySearchAccessTokenSettings = {
+      url: `https://accounts.spotify.com/api/token`,
+      options: {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/x-www-form-urlencoded",
+        },
+        body: `grant_type=client_credentials&client_id=${clientId}&client_secret=${clientSecret}`,
+      },
+    };
+
+    const spotitfySearchAccessToken = await getAccessToken.search({
+      url: spotifySearchAccessTokenSettings.url,
+      options: spotifySearchAccessTokenSettings.options,
+      isFetching: {
+        name: "apiKeys",
+        state: isFetching.apiKeys,
+      },
+      setIsFetching,
     });
+
+    setAccessTokens((prevT) => ({
+      ...prevT,
+      spotify_search: spotitfySearchAccessToken,
+    }));
+
+    const authHref = getAuthorizationUrl({
+      scopes: "playlist-modify-public",
+      client_id: clientId,
+      redirect_uri: "http://localhost:3000/callback",
+      response_type: "code",
+    });
+
+    const a = document.createElement("a");
+    a.href = authHref;
+    a.target = "_blank";
+    a.click();
+
+    // const aCode = await getSpotifyCodeForAccess();
+    // const aToken = await getAccessToken.search();
+
+    let isPreparingFile = true;
+    let attempts = 0;
+
+    try {
+      while (isPreparingFile && attempts <= 100) {
+        const res = await fetch("http://localhost:3000/isFileReady");
+        await delayMS(100);
+        attempts++;
+
+        if (res.status === 200) break;
+      }
+
+      const codeRes = await fetch("http://localhost:3000/code");
+      const codeData = await codeRes.json();
+
+      const tokenRes = await fetch("http://localhost:3000/token", {
+        method: "POST",
+        body: JSON.stringify({
+          client_id: clientId,
+          client_secret: clientSecret,
+          redirect_uri: "http://localhost:3000/callback",
+          code: codeData.code,
+        }),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+
+      const tokenData = await tokenRes.json();
+
+      const profile = await getUserProfile({
+        access_token: tokenData.token,
+        isFetching: {
+          name: "userProfile",
+          state: isFetching.userProfile,
+        },
+        setIsFetching,
+      });
+
+      setAccessTokens((prevA) => ({
+        ...prevA,
+        spotify_access: tokenData.token,
+      }));
+      setUserIds((prevU) => ({ ...prevU, spotify: profile.id }));
+      setIsAccessGranted(true);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const handleRefreshIconClick = () => {
+    setPlaylist([]);
+    setIsAccessGranted(false);
+    setSelectedTrackIds({});
   };
 
   useEffect(() => {
@@ -214,45 +269,74 @@ const App = () => {
   }, [selectedTrackIds]);
 
   return (
-    <Box className="relative w-full min-h-screen overflow-y-auto">
+    <Box className="relative w-full min-h-screen overflow-y-auto p-4">
       <Box
-        className="absolute top-0 left-0 -z-10 w-full h-full"
+        className="absolute top-0 left-0 -z-10 w-full h-full "
         sx={{
           backgroundColor: "rgb(136,136,136)",
           background:
             "linear-gradient(318deg, rgba(136,136,136,1) 6%, rgba(230,218,139,1) 66%)",
         }}
       ></Box>
-      <Box className="p-4 flex items-center flex-col gap-4">
-        <Typography variant="h3">PLAYLIST MAKER</Typography>
-        <Box className="flex gap-4 flex-row">
-          <TextField placeholder="CLIENT ID" ref={clientIdInputRef}></TextField>
-          <TextField
-            placeholder="CLIENT SECRET"
-            ref={clientSecretInputRef}
-          ></TextField>
-        </Box>
-        <TextField
-          placeholder="SONG NAME"
-          className="w-full"
-          onChange={handleSearchInputChange}
-          onKeyDown={handleSearchKeyDown}
-        ></TextField>
-        <Box className="grid grid-cols-2 gap-4 justify-between border border-black w-full p-4">
-          <Track
-            tracks={fetchedTracks}
-            selectedTrackIds={selectedTrackIds}
-            onTrackAdd={onTrackAdd}
-          />
+      <Box className='flex flex-row gap-4 justify-between '>
+        {isAccessGranted && (
           <Box>
-            <Playlist
-              playlist={playlist}
-              onTrackDelete={onTrackDelete}
-              onPlaylistUpload={onPlaylistUpload}
+            <FontAwesomeIcon
+              icon={faSyncAlt}
+              className="hover:cursor-pointer hover:text-red-700"
+              onClick={handleRefreshIconClick}
             />
           </Box>
-        </Box>
-        {isFetching && (
+        )}
+        <Typography variant="h4" className="text-center w-full">
+          PLAYLIST MAKER
+        </Typography>
+        <Box></Box>
+      </Box>
+
+      <Box className="flex items-center flex-col gap-4">
+        {!isAccessGranted && (
+          <Box className="flex flex-col gap-4">
+            <Box className="flex gap-4 flex-row">
+              <TextField
+                placeholder="CLIENT ID"
+                inputRef={clientIdInputRef}
+              ></TextField>
+              <TextField
+                placeholder="CLIENT SECRET"
+                inputRef={clientSecretInputRef}
+              ></TextField>
+            </Box>
+            <Box className="flex flex-col">
+              <Button onClick={handleGrantAccessClick}>GRANT ACCESS</Button>
+            </Box>
+          </Box>
+        )}
+        {isAccessGranted && (
+          <Box className="w-full flex flex-col gap-4">
+            <TextField
+              placeholder="SEARCH SONG NAME"
+              className="w-full"
+              onChange={handleSearchInputChange}
+              onKeyDown={handleSearchKeyDown}
+            ></TextField>
+            <Box className="grid grid-cols-2 gap-4 justify-between border border-black w-full p-4">
+              <Track
+                tracks={fetchedTracks}
+                selectedTrackIds={selectedTrackIds}
+                onTrackAdd={onTrackAdd}
+              />
+              <Box>
+                <Playlist
+                  playlist={playlist}
+                  onTrackDelete={onTrackDelete}
+                  onPlaylistUpload={onPlaylistUpload}
+                />
+              </Box>
+            </Box>
+          </Box>
+        )}
+        {Object.values(isFetching).find((value) => value) && (
           <Box className="fixed left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2">
             <CircularProgress></CircularProgress>
           </Box>
